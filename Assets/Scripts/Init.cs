568f2711263ea321063ef68fd4a0c22f6b647efb
CocoaCaa <55355513+CocoaCaa@users.noreply.github.com>
2021-07-25 12:39:20 +0800

    feat: enable vSync

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    void Start()
+    {
+        QualitySettings.vSyncCount = 1;
+    }
+}
